c5d63115228a90d7a7128a315d5c265c8ebd903a
gustavo <gustsanc@gmail.com>
2021-12-08 18:55:09 +0100

    AoC 2021 / day03 - part2

--- src/main/scala/com/gsanchez/aoc/year2021/Day03.scala
+++ src/main/scala/com/gsanchez/aoc/year2021/Day03.scala
@@ -5,9 +5,23 @@ import com.gsanchez.util.Files.read
 object Day03 {
 
   class PowerConsumption(gamma: Int) {
-    val epsilon = 0x00000FFF^gamma
+    val epsilon = 0x00000FFF ^ gamma
 
     def power: BigInt = gamma * epsilon
+  }
+
+  class LifeSupportRating(oxygen: Int, co2: Int) {
+    def rating = oxygen * co2
+  }
+
+  private def mostFreqBit(report: Seq[String], bitPos: Int): Char = {
+    val numOf1 = report.map(line => line.charAt(bitPos))
+    if (numOf1.count(_ == '0') > report.size / 2) '0' else '1'
+  }
+
+  private def leastFreqBit(report: Seq[String], bitPos: Int): Char = {
+    val numOf1 = report.map(line => line.charAt(bitPos))
+    if (numOf1.count(_ == '0') > report.size / 2) '1' else '0'
   }
 
   def part1(report: Seq[String]): PowerConsumption = {
@@ -18,8 +32,23 @@ object Day03 {
     new PowerConsumption(Integer.parseInt(num, 2))
   }
 
+  def part2(report: Seq[String]): LifeSupportRating = {
+    val numBitSize = report.head.size
+    val oxygen = (0 until numBitSize).foldLeft(report)((elems, bitindex) => {
+      val bit = mostFreqBit(elems, bitindex)
+      if (elems.size > 1) elems.filter(line => line.charAt(bitindex) == bit) else elems
+    })
+    val co2 = (0 until numBitSize).foldLeft(report)((elems, bitindex) => {
+      val bit = leastFreqBit(elems, bitindex)
+      if (elems.size > 1) elems.filter(line => line.charAt(bitindex) == bit) else elems
+    })
+
+    new LifeSupportRating(Integer.parseInt(oxygen.head, 2), Integer.parseInt(co2.head, 2))
+  }
+
   def main(args: Array[String]): Unit = {
     val report = read("day03.txt")
     println(s"Power Consumption:  ${part1(report).power}")
+    println(s"Life Supporting Rating:  ${part2(report).rating}")
   }
 }
