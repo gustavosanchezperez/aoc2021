170ccef0882339a524cecffb731b134e821feec4
gustavo <gustsanc@gmail.com>
2021-12-08 12:42:09 +0100

    AoC 2021 / day01 - gardening

--- src/main/scala/com/gsanchez/aoc/year2021/Day01.scala
+++ src/main/scala/com/gsanchez/aoc/year2021/Day01.scala
@@ -4,23 +4,20 @@ import com.gsanchez.util.Files.{read, readAsInt}
 
 object Day01 {
 
-  def part1(resource: String): Unit = {
-    val lines = readAsInt(resource)
-
-    val largerThanPrev = lines.sliding(2).map(pair => (pair, pair.last - pair.head)).count(_._2 > 0)
-    println(s"Counter Increased> $largerThanPrev")
+  private def depthMeasurement(lines: Seq[Int]): Int = {
+    lines.sliding(2).map(pair => (pair, pair.last - pair.head)).count(_._2 > 0)
   }
 
-  def part2(resource: String): Unit = {
-    val lines = readAsInt(resource)
+  def part1(lines: Seq[Int]): Int = depthMeasurement(lines)
 
-    val largerThanPrev = lines.sliding(3).map(_.sum).sliding(2).map(pair => (pair, pair.last - pair.head)).count(_._2 > 0)
-    println(s"Counter Increased> $largerThanPrev")
+  def part2(lines: Seq[Int]): Int = {
+    depthMeasurement(lines.sliding(3).map(_.sum).toSeq)
   }
 
 
   def main(args: Array[String]): Unit = {
-    part1("day01.part01.txt")
-    part2("day01.part01.txt")
+    val input = readAsInt("day01.part01.txt")
+    println(s"Counter Increased> ${part1(input)}")
+    println(s"Counter Increased> ${part2(input)}")
   }
 }
