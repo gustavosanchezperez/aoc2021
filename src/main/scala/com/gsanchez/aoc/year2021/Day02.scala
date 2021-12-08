2e1edd62c317e8ed89566103e3856b14fd4fa434
gustavo <gustsanc@gmail.com>
2021-12-08 13:25:32 +0100

    AoC 2021 / day02 - part2

--- src/main/scala/com/gsanchez/aoc/year2021/Day02.scala
+++ src/main/scala/com/gsanchez/aoc/year2021/Day02.scala
@@ -3,7 +3,11 @@ package com.gsanchez.aoc.year2021
 import com.gsanchez.util.Files.readAsPair
 
 object Day02 {
-  class Position(val hor: Int, val depth: Int) {
+  class Position(val hor: Int, val depth: Int, val aim: Int) {
+    def this(hor: Int, depth: Int) {
+      this(hor, depth, 0)
+    }
+
     def value: Int = hor * depth
   }
 
@@ -18,10 +22,22 @@ object Day02 {
     })
   }
 
+  def part2(course: Seq[(String, Int)], initial: Position): Position = {
+    course.foldLeft(initial)((p: Position, m: (String, Int)) => {
+      val position = m match {
+        case ("forward", forward) => new Position(p.hor + forward, p.depth + (p.aim * forward), p.aim)
+        case ("down", down) => new Position(p.hor, p.depth, p.aim + down)
+        case ("up", up) => new Position(p.hor, p.depth, p.aim - up)
+      }
+      position
+    })
+  }
+
   def main(args: Array[String]): Unit = {
     val course = readAsPair("day02.txt")
-    course.foreach(println(_))
     val finalPos = part1(course, new Position(0, 0))
-    println(s"Final Position: ${finalPos} Result:${finalPos.value}")
+    println(s"Final Position: ${finalPos} Result: ${finalPos.value}")
+    val finalPos2 = part2(course, new Position(0, 0, 0))
+    println(s"Final Position2: ${finalPos2} Result: ${finalPos2.value}")
   }
 }
