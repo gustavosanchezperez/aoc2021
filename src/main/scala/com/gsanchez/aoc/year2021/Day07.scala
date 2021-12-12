9d023f2cc1f861d0a5d244202d99c2be1d35b31b
gustavo <gustsanc@gmail.com>
2021-12-12 19:14:13 +0100

    AoC 2021 / day07 - part2

--- src/main/scala/com/gsanchez/aoc/year2021/Day07.scala
+++ src/main/scala/com/gsanchez/aoc/year2021/Day07.scala
@@ -4,42 +4,59 @@ import com.gsanchez.util.Files.{read, readAsInt}
 
 object Day07 {
 
-  def fuel(positions: List[Int], aligned: Int): Int = {
+  def fuelCheap(positions: List[Int], aligned: Int): Int = {
     positions.foldLeft(0)((fuel, pos) => fuel + Math.abs(aligned - pos))
   }
 
-  def part1(hpos: List[Int]): (Int, Int) = {
-    var deltax = (hpos.last - hpos.head) / 4
-    var x_start = hpos.sum / hpos.size
-    var dir = if (hpos.partition(_ == x_start)._1.size > hpos.size / 2) 1 else -1
-    var fuel_start = fuel(hpos, x_start)
-    var fuel_min = fuel_start
-    var x_min = x_start
+  def fuelExpensive(positions: List[Int], aligned: Int): Int = {
+    positions.foldLeft(0)((fuel, pos) => {
+      val x = Math.abs(aligned - pos)
+      val fuelAtThisPos = (x * (x + 1)) / 2
+      fuel + fuelAtThisPos
+    })
+  }
 
-    println(s"xs: ${x_start} -> fuels?${fuel_start}")
+  def part1(hpos: List[Int], fuel: (List[Int], Int) => Int): (Int, Int) = {
+    var deltax = (hpos.last - hpos.head) / 4
+    var x_current = hpos.sum / hpos.size
+    var dir = if (hpos.partition(_ == x_current)._1.size > hpos.size / 2) 1 else -1
+    var fuel_current = fuel(hpos, x_current)
+    var fuel_min = fuel_current
+    var x_min = x_current
+
+    println(s"xs: ${x_current} -> fuels?${fuel_current}")
     while (deltax > 0) {
-      x_start = x_start + dir * deltax
-      val fuel_next = fuel(hpos, x_start)
-      println(s"x: ${x_start} -> fuel?${fuel_next} delta(${dir * deltax})")
+      x_current = x_current + dir * deltax
+      val fuel_next = fuel(hpos, x_current)
+      println(s"x: ${x_current} -> fuel?${fuel_next} delta(${dir * deltax})")
       if (fuel_next < fuel_min) {
+        x_min = x_current
         fuel_min = fuel_next
-        x_min = x_start
       }
-      else {
-        deltax = Math.abs(x_start - x_min) / 2
-        dir = if (((x_min - x_start.toFloat) / (fuel_min - fuel_next.toFloat)) > 0) -1 else 1
+      if (fuel_next > fuel_current) {
+        deltax = Math.abs(x_current - x_min) / 2
+        dir = if (((x_min - x_current.toFloat) / (fuel_min - fuel_next.toFloat)) > 0) -1 else 1
       }
+      fuel_current = fuel_next
     }
     (x_min, fuel_min)
   }
 
   def main(args: Array[String]): Unit = {
-    val input = read("day07.txt").head
-//        val input = "16,1,2,0,4,2,7,1,2,14"
+        val input = read("day07.txt").head
+//    val input = "16,1,2,0,4,2,7,1,2,14"
     val horPos = input.split(",").map(_.toInt).sorted.toList
     println(horPos)
-    val hpos = part1(horPos)
+    val hpos = part1(horPos, fuelCheap)
     println(s"Align position ${hpos}")
-    println(s"i:${fuel(horPos, hpos._1-1)} s:${fuel(horPos, hpos._1)} d:${fuel(horPos, hpos._1+1)}")
+    println(s"i:${fuelCheap(horPos, hpos._1 - 1)} s:${fuelCheap(horPos, hpos._1)} d:${
+      fuelCheap(horPos, hpos._1
+        + 1)
+    }")
+
+    println(s"i:${fuelExpensive(horPos, 2)}")
+    val hpos2 = part1(horPos, fuelExpensive)
+    println(s"Align position ${hpos2}")
+    println(s"i:${fuelExpensive(horPos, hpos2._1 - 1)} s:${fuelExpensive(horPos, hpos2._1)} d:${fuelExpensive(horPos, hpos2._1 + 1)}")
   }
 }
