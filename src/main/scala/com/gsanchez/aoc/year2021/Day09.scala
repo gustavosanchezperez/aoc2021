36616a81f3fafdd3a64b76d5e3c02ff9f50777ba
gustavo <gustsanc@gmail.com>
2021-12-16 09:16:00 +0100

    AoC 2021 / day09 - part2

--- src/main/scala/com/gsanchez/aoc/year2021/Day09.scala
+++ src/main/scala/com/gsanchez/aoc/year2021/Day09.scala
@@ -4,31 +4,51 @@ import com.gsanchez.util.Files.read
 
 object Day09 {
 
-  def neighbourgs(input: List[Int], pos: Int, rowSz: Int): List[Int] = {
+  case class Location(pos: Int, height: Int) {}
+
+  def neighbourgs(input: List[Int], pos: Int, rowSz: Int): List[Location] = {
     val up = pos - rowSz
     val down = pos + rowSz
     val left = if (pos % rowSz == 0) -1 else pos - 1
     val right = if ((pos + 1) % rowSz == 0) -1 else pos + 1
     val positions = List(up, down, left, right)
-    val nbs = positions.foldLeft(List[Int]())((nb, npos) => {
-      if (input.indices.contains(npos)) input(npos) :: nb else nb
+    val nbs = positions.foldLeft(List[Location]())((nb, npos) => {
+      if (input.indices.contains(npos)) Location(npos, input(npos)) :: nb else nb
     })
     nbs
   }
 
-  def lowPoint(point: Int, neighbourgs: List[Int]): Boolean = neighbourgs.min > point
+  def lowPoint(point: Int, neighbourgs: List[Location]): Boolean = neighbourgs.map(_.height).min > point
+
+  def basin(loc: Location, hmap: List[Int], rs: Int): List[Int] = {
+    def loop(nb: List[Location], bs: Set[Location]): Set[Location] = {
+      if (nb.isEmpty) bs
+      else {
+        val newbasin = bs ++ nb
+        val newnb = nb.flatMap(n => {
+          neighbourgs(hmap, n.pos, rs).filter(_.height != 9).diff(newbasin.toList)
+        })
+        loop(newnb, newbasin)
+      }
+    }
+
+    loop(List[Location](loc), Set()).toList.map(_.height)
+  }
+
+  def part1(lp: List[Location]): Int = lp.map(n => n.height + 1).sum
+
+  def part2(lp: List[List[Int]]): Int = lp.map(_.size).sorted.takeRight(3).product
 
   def main(args: Array[String]): Unit = {
     val input = read("day09.txt")
     val rowSz = input.head.size
     val i2 = input.map(_.toList).flatMap(_.map(c => Integer.parseInt(c.toString))).toList
 
-    val lp = i2.zip(i2.indices).foldLeft(List[Int]())((lowPoints, point) => {
-      if (lowPoint(point._1, neighbourgs(i2, point._2, rowSz))) lowPoints :+ point._1 else lowPoints
+    val lp = i2.zipWithIndex.foldLeft(List[Location]())((lowPoints, point) => {
+      if (lowPoint(point._1, neighbourgs(i2, point._2, rowSz))) lowPoints :+ Location(point._2, point._1) else lowPoints
     })
 
-    val riskLevel = lp.map(n => n + 1)
-
-    println(s"risk: ${riskLevel.sum}")
+    println(s"risk: ${part1(lp)}")
+    println(s"basin product: ${part2(lp.map(basin(_, i2, rowSz)))}")
   }
 }
